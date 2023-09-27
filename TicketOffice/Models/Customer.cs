2c762a9cde8c43c8ab81e4677e06a9294abba915
Thord Möller <thord_moller_92@hotmail.com>
2023-09-27 04:48:01 +0200

    Defined the customer object

---
+++ TicketOffice/Models/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketOfficeAssignment.Models
+{
+    internal class Customer
+    {
+        //fields to be used only within this class
+        private int age;
+        private bool isTicketSeated;
+
+        //public getters and setters for outside access
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public bool IsTicketSeated
+        {
+            get { return isTicketSeated; }
+            set { isTicketSeated = value; }
+        }
+
+        //constructor. Set fields when creating the object
+        public Customer(int age, bool isTicketSeated) {
+            this.age = age;
+            this.isTicketSeated = isTicketSeated;
+        }
+
+    }
+}
--- TicketOffice/Services/TicketOffice.cs
+++ TicketOffice/Services/TicketOffice.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TicketOffice
+namespace TicketOfficeAssignment.Services
 {
     internal class TicketOffice
     {
