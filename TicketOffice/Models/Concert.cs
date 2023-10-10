efd1a121ce5a1acc6ce6a8db28a59eee9326f18d
Thord Möller <thord_moller_92@hotmail.com>
2023-10-10 06:11:42 +0200

    Assignment complete

---
+++ TicketOffice/Models/Concert.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketOfficeAssignment.Models
+{
+    public class Concert
+    {
+
+        public int Id { get; set; }
+        public bool ReducedVenue { get; set; }
+        public DateTime Date { get; set; }
+        public string Performer { get; set; }
+        public int BeginsAt { get; set; }
+        public int FullCapacitySales { get; set; }
+
+
+        public string ToString() {
+            return Id + " " + ReducedVenue + " " + Date.ToString() + " " + Performer + " " + BeginsAt + " " + FullCapacitySales;
+        }
+
+    }
+}
--- TicketOffice/Program.cs
+++ TicketOffice/Program.cs
@@ -1,13 +1,85 @@
-﻿namespace TicketOfficeAssignment
+﻿using System;
+using System.Text.Json;
+using TicketOfficeAssignment.Models;
+
+namespace TicketOfficeAssignment
 {
     internal class Program
     {
         static void Main(string[] args) {
-            TicketOffice.Initiate();
+            //TicketOffice.Initiate();
 
-            //Ticket ticket = new Ticket(24, TicketType.Seated);
+            string concertData = File.ReadAllText("C:\\Users\\thord\\source\\repos\\TicketOffice\\TicketOffice\\concert_data.json");
+            List<Concert> concerts = JsonSerializer.Deserialize<List<Concert>>(concertData);
 
-            //Console.WriteLine(ticket.Age + " " + ticket.Place + " " + ticket.Price() + " " + ticket.Tax());
+            PrintConcertList(concerts);
+
+            PrintConcertList(UpcomingConcerts(concerts));
+
+            PrintConcertList(ReducedVenues(concerts));
+
+            PrintConcertList(All2024(concerts));
+
+            PrintConcertList(FiveBiggestFullCapacitySales(concerts));
+
+            PrintConcertList(OnlyFriday(concerts));
+        }
+
+        private static void PrintConcertList(List<Concert> concerts) {
+            foreach(Concert concert in concerts) {
+                Console.WriteLine(concert.ToString());
+            }
+            Console.WriteLine();
+        }
+
+        private static List<Concert> UpcomingConcerts(List<Concert> concerts) {
+
+            List<Concert> sorted = concerts
+                .Where(concert => concert.Date >= DateTime.Now)
+                .OrderBy(concert => concert.Date)
+                .ToList();
+
+            return sorted;
+        }
+        //Return a new List<Concert> with all concerts of a ReducedVenue (true)
+        private static List<Concert> ReducedVenues(List<Concert> concerts) {
+
+            List<Concert> sorted = concerts
+                .Where(concert => concert.ReducedVenue == true)
+                .ToList();
+
+            return sorted;
+        }
+
+        //Return a new List<Concert> with all concerts during 2024.
+        private static List<Concert> All2024(List<Concert> concerts) {
+
+            List<Concert> sorted = concerts
+                .Where(concert => concert.Date.Year == 2024)
+                .ToList();
+
+            return sorted;
+        }
+
+        //Return a new List<Concert> with the five biggest projected sales figures (theFullCapacitySales value).
+        private static List<Concert> FiveBiggestFullCapacitySales(List<Concert> concerts) {
+
+            List<Concert> sorted = concerts
+                .OrderByDescending(x => x.FullCapacitySales)
+                .Take(5)
+                .ToList();
+
+            return sorted;
+        }
+
+        //Return a new List<Concert> with all concerts taking place on a Friday. The Date(because it is of DateTime) has a property called DayOfWeek.There is also an enumeration called the same thing, DayOfWeek.
+        private static List<Concert> OnlyFriday(List<Concert> concerts) {
+
+            List<Concert> sorted = concerts
+                .Where(concert => concert.Date.DayOfWeek == DayOfWeek.Friday)
+                .ToList();
+
+            return sorted;
         }
     }
 }
